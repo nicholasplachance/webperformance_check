f0802ce49ba21a85dce850078c24513f8a140dd7
nick lachance <nicholasplachance@gmail.com>
2024-05-09 00:40:03 -0500

    set up basic puppeteer script and ensure it is in a working state to display the title of the webpage intended to be visited

--- scraper/scraper.js
+++ scraper/scraper.js
@@ -1,9 +1,20 @@
-const puppeteer = require("puppeteer");
+const puppeteer = require('puppeteer');
 
-async function scrapePerformanceMetrics(url) {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(url);
-    // perform task similiar to python's selenium script
-    await browser.close();
-}
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  // Navigate to the page
+  await page.goto('https://hellhades.com');
+
+  // Get and print the page title
+  const title = await page.title();
+  console.log('Page title:', title);
+
+  // Get and print the URL of the page to ensure it is the intended page
+  const url = page.url();
+  console.log('URL visited:', url);
+
+  // Close the browser
+  await browser.close();
+})();
