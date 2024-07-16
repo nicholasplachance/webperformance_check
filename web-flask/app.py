9f10b0a8d634389b83877655e04db0ae6326fdab
nick lachance <nicholasplachance@gmail.com>
2024-07-16 18:32:24 -0500

    update app.py

--- web-flask/app.py
+++ web-flask/app.py
@@ -80,6 +80,4 @@ def report():
     return render_template('report.html', data=data, average_metrics=average_metrics)
 
 if __name__ == '__main__':
-    # Use Heroku's provided port or default to 5000
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(debug=True)
