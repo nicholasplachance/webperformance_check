3d788bfb8f1466c1184094d2237325f540759af9
nick lachance <nicholasplachance@gmail.com>
2024-07-16 18:16:45 -0500

    add port compatibility

--- web-flask/app.py
+++ web-flask/app.py
@@ -80,4 +80,6 @@ def report():
     return render_template('report.html', data=data, average_metrics=average_metrics)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Use Heroku's provided port or default to 5000
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
