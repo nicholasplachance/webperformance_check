0212a60570a630645b3741e7ceebac3a089a8aef
nick lachance <nicholasplachance@gmail.com>
2024-07-17 12:16:40 -0500

    add python procfile

--- run_scraper.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Ensure node is available
-NODE_PATH=${NODE_PATH:-$(which node)}
-SCRAPER_PATH=${SCRAPER_PATH:-./scraper/scraper.js}
-
-# Run the scraper script
-$NODE_PATH $SCRAPER_PATH $1
